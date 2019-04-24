6e3d98f4b69b355cf4964bfeb95b8df6cb375366
lilong <lilong_03@163.com>
2019-04-24 17:29:37 +0800

    修改11111

--- test.java
+++ test.java
@@ -1,8 +1,10 @@
 class  test{
     public static void main(){
         System.out.println("hello world");
+        System.out.println("hello world1");
     }
     pubic void test(){
         System.out.println("hello android");
+        System.out.println("hello andorid1111");
     }
 }
